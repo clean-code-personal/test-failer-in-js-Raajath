ddf0b0b5c6e16f50f08e59351627d86d54b99c86
Rajath .N <89772947+Raajath@users.noreply.github.com>
2024-02-12 09:22:27 +0530

    Update tshirts.js
    Removed Extra condition

--- tshirts.js
+++ tshirts.js
@@ -2,7 +2,7 @@
 function size(cms) {
     if (cms <= 38) {
         return 'S';
-    } else if (cms > 38 && cms < 42) {
+    } else if (cms < 42) {
         return 'M';
     } else {
         return 'L';
