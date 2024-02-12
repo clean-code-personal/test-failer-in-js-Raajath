6cb7b84de5444bca92b7207b70764ab36e517a8b
Rajath .N <89772947+Raajath@users.noreply.github.com>
2024-02-12 09:45:45 +0530

    Update stub.js
    Made 2 stubs for success and failure in transmission

--- tests/stub.js
+++ tests/stub.js
@@ -2,7 +2,6 @@ function networkTransmitStubSucess(celcius) {
     console.log(`Temperature to transmit: ${celcius} celcius`);
   return 200;
 }
-
 function networkTransmitStubFailure(celcius){
     console.log(`Temperature to transmit: ${celcius} celcius`);
    return 500;
